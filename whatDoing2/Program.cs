8fa2ff28afde401d9a2d6b199b8fe4b15edae0d5
makskayunPC <maks.kayn@yandex.com>
2021-09-20 13:39:54 +0300

    add Program.cs - main class

---
+++ whatDoing2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace whatDoing2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Person child = new Child("Петя Петров", 5); // Полиморфизм
+            Console.WriteLine(child.Name);
+            
+            Child othercChildhild = new Child("Ваня Иванов", 4);
+            othercChildhild.AddFriends(2);
+            Console.Write('\n' + othercChildhild.Name + " ");
+            othercChildhild.GoToKindergarten();
+            othercChildhild.Eat();
+        }
+    }
+}
